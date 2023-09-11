18fbe2475104bfb4a1234fc91745581151010004
Muntasir Hossen <montachirhossen76@gmail.com>
2023-09-11 22:37:32 +0600

    Contact Activity added (not working)

--- script.js
+++ script.js
@@ -59,3 +59,98 @@ circles.forEach(elem=>{
 
 // mix it up portfolio section
 var mixer = mixitup('.portfolio-gallery');
+
+// active menu
+let menuLi = document.querySelectorAll('header ul li a');
+let section = document.querySelectorAll('section');
+
+function activeMenu(){
+    let len = section.length;
+    while(--len && window.scrollY+97 < section[len].offsetTop){}
+    menuLi.forEach(sec => sec.classList.remove("active"));
+    menuLi[len].classList.add("active");
+}
+
+activeMenu();
+window.addEventListener("scroll", activeMenu);
+
+// sticky navbar ---------------------------
+const header = document.querySelectorAll("header");
+window.addEventListener("scroll", function(){
+    header.classList.toggle("sticky", window.scrollY > 50);
+})
+
+// toggle icon navbar ---------------------------
+let menuIcon = document.querySelector("#menu-icon");
+let navlist = document.querySelector(".navlist");
+
+menuIcon.onclick = () =>{
+    menuIcon.classList.toggle("bx-x");
+    navlist.classList.toggle("open");
+}
+
+menuIcon.onscroll = () =>{
+    menuIcon.classList.remove("bx-x");
+    navlist.classList.replace("open");
+}
+
+// parallax -------------------------------------
+
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add("show-items");
+        }else{
+            entry.target.classList.remove("show-items");
+        }
+
+    });
+});
+
+const scrollScale = document.querySelectorAll(".scroll-scale");
+scrollScale.forEach((el)=>observer.observe(el));
+
+const scrollBottom = document.querySelectorAll(".scroll-bottom");
+scrollBottom.forEach((el)=>observer.observe(el));
+
+const scrollTop = document.querySelectorAll(".scroll-top");
+scrollTop.forEach((el)=>observer.observe(el));
+
+// Email Activity
+
+
+function sendEmail(){
+    var params = {
+        from_name: document.getElementById("name").value,
+        email_id: document.getElementById("email").value,
+        message: document.getElementById("message").value,
+    };
+    const serviceID = "service_9uz66xb";
+    const templateID = "template_4sh8spk";
+    //  alert(params.email_id);
+     
+    emailjs.send(serviceID, templateID, params)
+        .then(function(response) {
+           console.log('SUCCESS!', response.status, response.text);
+           alert("Success");
+        }, function(error) {
+            alert("Failed..."+error);
+           console.log('FAILED...', error);
+        });
+
+    // emailjs.send(serviceID, templateID, params)
+    // .then(
+    //     res => {
+    //         document.getElementById("name").value = "";
+    //         document.getElementById("email").value = "";
+    //         document.getElementById("message").value = "";
+    //         console.log(res);
+    //         alert("Your message sent successfully");
+    // })
+    // .catch((err) => {
+    //     // console.log(err);
+    //     alert(err);
+    // });
+}
+
+
